6ee2eb42a3878c5dd897fc1993a5da2b31e1733c
Paolo Licup <jpmlicup10@gmail.com>
2022-05-26 09:19:51 +0800

    http module

--- myfirst.js
+++ myfirst.js
@@ -1,7 +1,12 @@
 var http = require('http');
+var url = require('url');
 
 // Creates a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); // Writes a response to the client
-  res.end(); // Ends the response
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  // res.write('Hello World!'); // Writes a response to the client
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;  
+  // res.write(req.url) // Displays the current URL
+  res.end(txt); // Ends the response
 }).listen(8080); // The server object listens on port 8080
