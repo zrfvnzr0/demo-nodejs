c7d11d0f5fa50ce3ee2ca104775f59b87386ceac
Paolo Licup <jpmlicup10@gmail.com>
2022-05-26 09:49:11 +0800

    events

---
+++ demo_events.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var rs = fs.createReadStream('./demofile.txt');
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+
+rs.on('open', function () {
+  console.log('The file is open');
+
+  //Create an event handler:
+  var myEventHandler = function () {
+    console.log('I hear a scream!');
+  }
+  
+  //Assign the event handler to an event:
+  eventEmitter.on('scream', myEventHandler);
+  
+  //Fire the 'scream' event:
+  eventEmitter.emit('scream');  
+
+});
