c214c37e1e9a837c8c05782f238ca6c0d85427ed
Paolo Licup <jpmlicup10@gmail.com>
2022-05-26 09:14:33 +0800

    demo module

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
+// Creates a server object:
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+  res.write('Hello World!'); // Writes a response to the client
+  res.end(); // Ends the response
+}).listen(8080); // The server object listens on port 8080
